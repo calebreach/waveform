39f9b127ec41a64437cee9096819ef44f8f689b8
Caleb Reach <caleb@acceliva.com>
2014-07-23 22:22:29 -0400

    sndfile

--- time-series.cpp
+++ time-series.cpp
@@ -1,62 +1,52 @@
 #include <iostream>
-
+#include <sndfile.hh>
 #include <math.h>
 #include "image.h"
 #include "lanczos.h"
 
 using namespace std;
 
-static void test_lanczos_fns() {
-  cout << lanczos(2.4234) << endl;
-  cout << lanczos(1.4234, 2) << endl;
-
-  cout << lanczosIntegral(2.4234) << endl;
-  cout << lanczosIntegral(1.4234, 2) << endl;
-}
-
-static void test_image() {
- Image img(100, 100);
-  img.add(15, 20, 0.5f);
-  img.add(40, 10, 1.0f);
-  img.add(60, 70, 1.5f);
-}
-
-static void test_lanczos_rast_x(Image& img) {
-  LanczosRasterizer rast(3);
-  for (int i=0; i<10; i++)
-    rast.drawLine(img, 40 + i*0.2, 4 + i*9, 10 + i*9);
-}
-
-static void test_lanczos_rast_y(Image& img) {
-  LanczosRasterizer rast(3);
-  for (int i=0; i<10; i++)
-    rast.drawLine(img, 4 + i*9, 20 + i*0.2, 50 + i*0.2);
-}
-
 int main() {
-  Image img(100, 100);
-
+  int width = 800;
+  int height = 800;
+  Image img(width, height);
   LanczosRasterizer rast(3);
-  // rast.drawLine(img,10,20,30);
-  // for (int i=0; i<10; i++)
-  //   rast.drawLine(img, 4 + i*9, 20, 20 + i*0.5f);
 
-  // rast.drawLine(img, 30, 20, 20);
-  // rast.drawLine(img, 50, 20, 25);
-  // rast.drawLine(img, 60, 20, 30);
-
-  float last = 0;
-  for (int i=0; i<10000; i++) {
-    float pos = i/10000.0f;
-    // float cur = sin(pos*M_PI*2*20);
-    float cur = (i & (1 << 1)) ? 1 : -1;
-    const float scale = 30;
-    float y1 = 50 + last*scale;
-    float y2 = 50 + cur*scale;
-    rast.drawLine(img, 10 + pos*80, y1, y2);
-    last = cur;
+  SndfileHandle sndfile("in.flac");
+  if (!sndfile) {
+    cerr << "couldn't find input file" << endl;
+    return 1;
   }
+  const int numChannels = sndfile.channels();
+  const int size = sndfile.frames()*numChannels;
+  const int bufSize = sndfile.samplerate()*numChannels;
+  float* buffer = new float[bufSize];
+  int numRead;
+  float last = 0;
+  int start = 0;
+  while ((numRead = sndfile.read(buffer, bufSize))) {
+    cout << "second" << endl;
+    for (int i=0; i<numRead; i+=numChannels) {
+      float pos = (start+i)/(float)size;
+      float cur = 0.5f*height + 0.4f*height*buffer[i];
+      rast.drawLine(img, pos*width, last, cur);
+      last = cur;
+    }
+    start += numRead;
+  }
+  delete buffer;
 
+  // float last = 0;
+  // for (int i=0; i<10000; i++) {
+  //   float pos = i/10000.0f;
+  //   // float cur = sin(pos*M_PI*2*20);
+  //   float cur = (i & (1 << 1)) ? 1 : -1;
+  //   const float scale = 30;
+  //   float y1 = 50 + last*scale;
+  //   float y2 = 50 + cur*scale;
+  //   rast.drawLine(img, 10 + pos*80, y1, y2);
+  //   last = cur;
+  // }
 
   img.integrateY();
   img.savePNG("out.png");
