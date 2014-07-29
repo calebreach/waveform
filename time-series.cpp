d3e3399d3d542a4f067cd49eb8ad45dabbc4cb37
Caleb Reach <caleb@acceliva.com>
2014-07-29 10:55:22 -0400

    added command line options

--- time-series.cpp
+++ time-series.cpp
@@ -1,18 +1,58 @@
 #include <iostream>
 #include <sndfile.hh>
 #include <math.h>
+#include <boost/program_options.hpp>
+
 #include "image.h"
 #include "lanczos.h"
 
 using namespace std;
 
-int main() {
-  int width = 1600;
-  int height = 480;
+namespace po = boost::program_options;
+
+int main(int argc, char** argv) {
+  int width, height;
+  string in_path, out_path;
+
+  po::options_description desc("Allowed options");
+
+  desc.add_options()
+    ("help", "show help")
+    ("width,w", po::value<int>(&width)->default_value(1600), "image width")
+    ("height,h", po::value<int>(&height)->default_value(480), "image height")
+    ("out,o", po::value<string>(&out_path)->required(), "png output")
+    ("input-file", po::value<string>(&in_path)->required(), "audio input");
+
+  po::positional_options_description p;
+  p.add("input-file", 1);
+
+  po::variables_map vm;
+
+  try {
+    po::store(po::command_line_parser(argc, argv)
+                .options(desc).positional(p).run(),
+              vm);
+    po::notify(vm);
+  } catch(po::error& e) {
+    std::cerr << "Error: " << e.what() << std::endl << std::endl; 
+    std::cerr << desc << std::endl; 
+    return 1; 
+  }
+
+  if (vm.count("help")) {
+    cerr << desc << endl;
+    return 1;
+  }
+
+  if (width < 0 || height < 0) {
+    cerr << "width and height should be positive" << endl;
+    return 1;
+  }
+
   Image img(width, height);
   LanczosRasterizer rast(3);
 
-  SndfileHandle sndfile("in-dnb.flac");
+  SndfileHandle sndfile(in_path);
   if (!sndfile) {
     cerr << "couldn't find input file" << endl;
     return 1;
@@ -51,11 +91,11 @@ int main() {
 
   img.integrateY();
   img.integrateYToMiddle();
-  float low = img.quantile(0.02);
-  float high = img.quantile(0.98);
+  // float low = img.quantile(0.02);
+  // float high = img.quantile(0.98);
   // img.log(0.1);
   img.clip(0,INFINITY);
   img.normalize();
   // img.sqrt();
-  img.savePNG("out.png");
+  img.savePNG(out_path);
 }
